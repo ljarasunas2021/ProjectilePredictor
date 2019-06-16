ef7e13838fefb904d1f1e96ef0c10e7d77d79178
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-06-16 09:16:44 -0700

    feat: Add working turret

--- Assets/Scenes/EnemyShip.cs
+++ Assets/Scenes/EnemyShip.cs
@@ -3,27 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyShip : MonoBehaviour {
-
-    // speed of enemy ship
-    public float speed;
-    // rigidbody of enemy ship
-    private Rigidbody rigidbody;
-
 	// Use this for initialization
 	void Start () {
-        // get rigidbody
-        rigidbody = GetComponent<Rigidbody>();
-
         // create initial random rotation
         float xRot = Random.Range(0, 360);
         float yRot = Random.Range(0, 360);
         float zRot = Random.Range(0, 360);
         gameObject.transform.rotation = Quaternion.Euler(new Vector3(xRot, yRot, zRot));
-    }
-
-    private void FixedUpdate() {
-        // set the rigidbodies' velocity to its local transform.right axis times speed
-        // means the rigidbody will travel to its right at a given speed
-        rigidbody.velocity = transform.right * speed;
     }
 }
--- Assets/Scenes/ProjectilePredictor.cs
+++ Assets/Scenes/ProjectilePredictor.cs
@@ -3,29 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectilePredictor : MonoBehaviour {
-    // projectile you want to mark
+    public GameObject turret;
     public GameObject projectile;
-    // a marker which shows where the projectile will be in secondsToPredict seconds
-    public GameObject marker;
-    // amount of second script should look ahead to see where it will be
-    public float secondsToPredict;
+    public float projectileSpeed;
+    public float secondsInBetweenShots;
+    public GameObject enemyShip;
+    public float enemyShipSpeed;
+
+    private Rigidbody enemyShipRigidBody;
 
     private void Start() {
-        StartCoroutine(CreateMarker());
+        enemyShipRigidBody = enemyShip.GetComponent<Rigidbody>();
+        StartCoroutine(ShootProjectile());
     }
 
-    // create a marker
-    private IEnumerator CreateMarker() {
-        // every 1 seconds create a marker
-        yield return new WaitForSeconds(1);
-        // find the next position
-        // **** what you will want to implement in the project****
-        Vector3 nextPos = projectile.transform.position + projectile.GetComponent<Rigidbody>().velocity * secondsToPredict;
-        // instantiate a new marker at the position the projectile will be in secondsToPredict seconds
-        GameObject newMarker = Instantiate(marker, nextPos, Quaternion.Euler(new Vector3(0, 0, 0)));
-        // destroy the marker in secondsToPredict seconds or when the projectile should be at that position
-        Destroy(newMarker, secondsToPredict);
-        // redo the courutine
-        StartCoroutine(CreateMarker());
+    // set velocity of ship to be its local right direction times its speed
+    private void FixedUpdate()
+    {
+        enemyShipRigidBody.velocity = enemyShip.transform.right * enemyShipSpeed;
+    }
+
+    private IEnumerator ShootProjectile() {
+        yield return new WaitForSeconds(secondsInBetweenShots);
+
+        // Math behind the projectiles
+        // ok so imagine a triangle with 3 vertices: the turret position, the current enemy ship position, and the future enemy ship position
+        // using the law of sins
+        // sin(currentShipPosAngle) / distance(turretPos, futureShipPos) = sin(futureShipPosAngle) / distance(shipPos,turretPos) = sin(180 - currentShipPosAngle - futureShipPosAngle) / distance(currentShipPos, futureShipPos)
+
+        // from the law of sins we can get the following
+        // distance(turretPos, futureShipPos) = distance(currentShipPos,turretPos) * sin(currentShipPosAngle) / sin(futureShipPosAngle)
+        // distance(currentShipPos, futureShipPos) = distance(shipPos, turretPos) * sin(180 - currentShipPosAngle - futureShipPosAngle) / sin(futureShipPosAngle)
+
+        // so the question is how can we make it so that the ship's and projectile's trajectories intersect at the same time
+        // since distance = speed * time, time = distance / speed so:
+        // distance(turretPos, futureShipPos) / turretVelo = distance(currentShipPos, futureShipPos) / shipVelo
+        // using TurretVelo as speed of projectile fired by turret and shipVelo as speed of ship
+
+        // by simplifying and plugging in stuff:
+        // distance(shipPos,turretPos) * sin(currentShipPosAngle) / sin(futureShipPosAngle) * turretVelo = distance(shipPos, turretPos) * sin(180 - currentShipPosAngle - futureShipPosAngle) / sin(futureShipPosAngle) * shipVelo
+        // turretVelo^(-1) * sin(currentShipPosAngle) / sin(futureShipPosAngle) = shipVelo^(-1) * sin(180 - currentShipPosAngle - futureShipPosAngle) / sin(futureShipPosAngle) 
+        // sin(180 - currentShipPosAngle - futureShipPosAngle) = shipVelo * sin(currentShipPosAngle) / turretVelo
+        // 180 - currentShipPosAngle - futureShipPosAngle = invSin(shipVelo * sin(currentShipPosAngle) / turretVelo)
+        // futureShipPosAngle = 180 - currentShipPosAngle - invSin(shipVelo * sin(currentShipPosAngle) / turretVelo)
+        // now we can find the futureShipPosAngle using measurements that we know
+        // this will be implemented below
+
+        // fakeFutureEnemyPosPoint: if the currentShipPos is the origin, fake future enemy pos point is a directional vector of where the future position will be
+        Vector3 fakeFutureEnemyPosPoint = enemyShipRigidBody.velocity;
+        // fakeTurretPos: turret position if the currentShipPos is the origin
+        Vector3 fakeTurretPos = turret.transform.position - enemyShip.transform.position;
+        // find the currentShipPosAngle using https://www.analyzemath.com/stepbystep_mathworksheets/vectors/vector3D_angle.html formula
+        float currentShipPosAngle = Mathf.Acos(Vector3.Dot(fakeFutureEnemyPosPoint, fakeTurretPos) / (fakeFutureEnemyPosPoint.magnitude * fakeTurretPos.magnitude));
+
+        // following is in radians due to Unity's obsession with radians
+        // using formula found above, find futureShipPosAngle
+        float futureShipPosAngle = Mathf.PI - currentShipPosAngle - Mathf.Asin(enemyShipSpeed * Mathf.Sin(currentShipPosAngle) / projectileSpeed);
+        // using shipLength or distance(currentShipPos, futureShipPos), we can find it using equation found above
+        float shipLength = Vector3.Distance(enemyShip.transform.position, turret.transform.position) * Mathf.Sin(Mathf.PI - currentShipPosAngle - futureShipPosAngle) / Mathf.Sin(futureShipPosAngle);
+        // using shipLength and currentShipPos we can find the position where the projectile and it will intersect
+        // since shipLength = distance & enemyShipSpeed = speed, shipLength / enemyShipSpeed is time:
+        Vector3 realEndPoint = enemyShip.transform.position + enemyShipRigidBody.velocity * shipLength / enemyShipSpeed;
+
+        // instantiate projectile
+        // make it look at realEndPoint
+        // set it along its velocity
+        GameObject projectile1 = Instantiate(projectile, turret.transform.position, Quaternion.Euler(new Vector3(0,0,0)));
+        projectile1.transform.LookAt(realEndPoint);
+        projectile1.GetComponent<Rigidbody>().velocity = projectile1.transform.forward * projectileSpeed;
+
+        // shoot another projectile
+        StartCoroutine(ShootProjectile());
     }
 }
